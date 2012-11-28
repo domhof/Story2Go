9ad8d864a582347a30829dd412731d33d295a386
Ulrich Lehner <me@ulrichlehner.com>
2012-11-29 00:54:48 +0100

    Changed ServiceIntent to Service in order to get LocationManager working.

--- src/at/ac/tuwien/igw/story2go/MainActivity.java
+++ src/at/ac/tuwien/igw/story2go/MainActivity.java
@@ -1,26 +1,27 @@
 package at.ac.tuwien.igw.story2go;
 
-import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
+	public static final String TAG = MainActivity.class.getSimpleName();
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        
-        Intent intent = new Intent(this, Story2GoService.class);
-        startService(intent);
-    }
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.activity_main, menu);
-        return true;
-    }
-    
+		Intent intent = new Intent(this, Story2GoService.class);
+		startService(intent);
+	}
+
+	@Override
+	public boolean onCreateOptionsMenu(Menu menu) {
+		// Inflate the menu; this adds items to the action bar if it is present.
+		getMenuInflater().inflate(R.menu.activity_main, menu);
+		return true;
+	}
+
 }
--- src/at/ac/tuwien/igw/story2go/Story2GoService.java
+++ src/at/ac/tuwien/igw/story2go/Story2GoService.java
@@ -2,75 +2,92 @@ package at.ac.tuwien.igw.story2go;
 
 import java.util.ArrayList;
 
-import android.app.IntentService;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
-import android.location.LocationProvider;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.util.Log;
 
-public class Story2GoService extends IntentService {
-	public static final String TAG = Story2GoService.class.getSimpleName(); 
+public class Story2GoService extends Service implements LocationListener {
+	public static final String TAG = Story2GoService.class.getSimpleName();
+
 	private LocationManager locationManager;
-	private LocationListener locationListener;
-		
-	public Story2GoService() {
-		super("Story2GoService");
-		Log.d(TAG, "start service");
-		
-		//initLocationAudio();
-		
-		locationManager = (LocationManager) this.getSystemService(Context.LOCATION_SERVICE);
-		
-//		
-//		locationListener = new LocationListener() {
-//			@Override
-//		    public void onLocationChanged(Location location) {
-//				
-//		    }
-//
-//		    public void onStatusChanged(String provider, int status, Bundle extras) {}
-//
-//		    public void onProviderEnabled(String provider) {}
-//
-//		    public void onProviderDisabled(String provider) {}
-//		  };
-//
-//		// Register the listener with the Location Manager to receive location updates
-//		locationManager.requestLocationUpdates(LocationManager.NETWORK_PROVIDER, 0, 0, locationListener);
-	}
+	private Location currentLocation;
 
 	private void initLocationAudio() {
 		ArrayList<LocationAudio> locations = new ArrayList<LocationAudio>();
 		Location l = new Location("testlocation");
 		l.setAltitude(10.0);
 		l.setLongitude(20.0);
-		locations.add(new LocationAudio(l,"testFile"));
-		
+		locations.add(new LocationAudio(l, "testFile"));
+
 		SharedData.setLocations(locations);
 	}
 
+	// @Override
+	// protected void onHandleIntent(Intent intent) {
+	// locationManager = (LocationManager)
+	// getSystemService(Context.LOCATION_SERVICE);
+	// locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+	// 0, this);
+	//
+	// // it is also possible to register an additional provider
+	// // locationManager.requestLocationUpdates(
+	// // LocationManager.NETWORK_PROVIDER, 0, 0, this);
+	//
+	// currentLocation = locationManager
+	// .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	// // initLocationAudio();
+	//
+	// while (true) {
+	// // Location nextLocation = SharedData.getNextLocation().location;
+	// // Location currentLocation = locationManager
+	// // .getLastKnownLocation(LocationManager.GPS_PROVIDER);
+	//
+	// // float distanceTo = nextLocation.distanceTo(currentLocation);
+	// // float distanceTo = 0;
+	// Log.d(TAG, "Current location (from Service): " + currentLocation);
+	// try {
+	// Thread.sleep(1000);
+	// } catch (InterruptedException e) {
+	// e.printStackTrace();
+	// }
+	// }
+	// }
+
 	@Override
-	protected void onHandleIntent(Intent intent) {
-		
-//		while (true) {
-//			Location nextLocation = SharedData.getNextLocation().location;
-//			//Location currentLocation = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
-//			
-//			//float distanceTo = nextLocation.distanceTo(currentLocation);
-//			float distanceTo = 0;
-//			Log.d(TAG, "next location: " + distanceTo);
-//			try {
-//				Thread.sleep(1000);
-//			} catch (InterruptedException e) {
-//				e.printStackTrace();
-//			}
-//		}
+	public void onCreate() {
+		super.onCreate();
+		locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+		locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0,
+				0, this);
 	}
 
+	@Override
+	public void onLocationChanged(Location location) {
+		currentLocation = location;
+		Log.d(TAG, "Current location: " + currentLocation);
+	}
+
+	@Override
+	public void onProviderDisabled(String provider) {
+	}
+
+	@Override
+	public void onProviderEnabled(String provider) {
+	}
+
+	@Override
+	public void onStatusChanged(String provider, int status, Bundle extras) {
+	}
+
+	@Override
+	public IBinder onBind(Intent intent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
